f6dd79515bba61c2da643a0821299d6aef89c650
Robert Houck <57123769+rbhouck32@users.noreply.github.com>
2020-02-21 02:01:09 -0500

    This commit is the completion of the MVP of this project

--- team-builder-app/src/components/MemberCard.js
+++ team-builder-app/src/components/MemberCard.js
@@ -2,10 +2,11 @@ import React from "react";
 
 const MemberCard = props => {
   return (
-    <div className="mamber-card">
+    <div className="member-card">
       {props.members.map(member => (
         <div className="member" key={member.id}>
           <h2>{member.name}</h2>
+          <p>{member.email}</p>
           <p>{member.role}</p>
         </div>
       ))}
--- team-builder-app/src/components/MemberForm.js
+++ team-builder-app/src/components/MemberForm.js
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
 
+var createReactClass = require("create-react-class");
+
 const MemberForm = props => {
   const [member, setMember] = useState({ name: "", email: "", role: "" });
-
-  const handleChanges = event => {
+  // handle change
+  const handleChange = event => {
     console.log("event", event.target.value);
     setMember({ ...member, [event.target.name]: event.target.value });
   };
 
-  const submitForm = event => {
+  // handle submit
+  const handleSubmit = event => {
     event.preventDefault();
     props.addNewMember(member);
     setMember({ name: "", email: "", role: "" });
   };
 
+  // const DropDownSelctor = React.createClass({
+  //   getInitialState: function() {
+  //     return { selectValue: "" };
+  //   },
+  //   handleChange: function(e) {
+  //     this.setState({ selectValue: e.target.value });
+  //   }
+  // });
+
   return (
-    <form onSubmit={submitForm}>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="name">Name</label>
 
       <input
@@ -24,7 +36,7 @@ const MemberForm = props => {
         name="name"
         value={member.name}
         placeholder="Enter your Name"
-        onChange={handleChanges}
+        onChange={handleChange}
       />
       <br />
 
@@ -35,17 +47,30 @@ const MemberForm = props => {
         name="email"
         value={member.email}
         placeholder="Enter your Email"
-        onChange={handleChanges}
+        onChange={handleChange}
       />
       <br />
-      <select name="team-roles" id="role">
+
+      {/* <label htmlFor="role">Role</label>
+      <input
+        type="text"
+        id="role"
+        name="role"
+        value={member.role}
+        placeholder="Enter your Role"
+        onChange={handleChanges}
+      /> */}
+
+      <select name="role" id="role" onChange={handleChange}>
         <option value="">--Please choose a role--</option>
         <option value="backend-engineer">Backend Engineer</option>
         <option value="frontend-engineer">Frontend Engineer</option>
         <option value="designer">Designer</option>
-        <option value="DS">Data Science Nerd</option>
+        <option value="Data Science Nerd">Data Science Nerd</option>
         <option value="FS-dev">Full-Stack Engineer</option>
       </select>
+
+      <br />
       <br />
       <button type="submit" value="Submit Form">
         Submit Form
